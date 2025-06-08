3c9b87538818c5534c4822dc063877a20ed1d986
Matteo <matteojandrade@gmail.com>
2025-06-08 11:05:00 -0400

    Added search function

--- movie_rec.py
+++ movie_rec.py
@@ -1,13 +1,28 @@
 import pandas as pd
 import re
-
-movies =  pd.read_csv("movies.csv")
-ratings = pd.read_csv("ratings.csv")
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 def clean(title):
     return re.sub("[^a-zA-Z0-9 ]", "", title)
 
+movies =  pd.read_csv("movies.csv")
+ratings = pd.read_csv("ratings.csv")
+
 movies["cleaned"] = movies["title"].apply(clean)
 
-print(movies["title"][0])
-print(movies["cleaned"][0])
+vector = TfidfVectorizer(ngram_range=(1,2))
+matrix = vector.fit_transform(movies["cleaned"])
+
+def search(term):
+    cleaned = clean(term)
+    vec = vector.transform([cleaned])
+    sim = cosine_similarity(vec, matrix).flatten()
+    pos = np.argpartition(sim, -8)[-8:]
+    res = movies.iloc[pos][::-1]
+    return res
+
+search_term = input("Hello, what movie would you like to search for?\n")
+print("Here are the results:")
+print(search(search_term)["title"])
