563be33a0b940b70c9cd3927f912341780aded0d
Matteo <matteojandrade@gmail.com>
2025-06-10 19:45:04 -0400

    Assigned scores to all movies

--- movie_rec.py
+++ movie_rec.py
@@ -25,14 +25,22 @@ def search(term):
 
 def recommend(term):
     id = 1
-    simUsers = ratings[(ratings["movieId"] == id) & (ratings["rating"] >= 4)]["userId"].unique()
-    sim_recs = ratings[(ratings["userId"].isin(simUsers)) & (ratings["rating"] >= 4)]["movieId"]
-    sim_recs = sim_recs.value_counts() / len(sim_recs)
+    simUsers = ratings[(ratings["movieId"] == id) & (ratings["rating"] >= 5)]["userId"].unique()
+    sim_recs = ratings[(ratings["userId"].isin(simUsers)) & (ratings["rating"] > 4)]["movieId"]
+    sim_recs = sim_recs.value_counts() / len(simUsers)
     sim_recs = sim_recs[sim_recs > .1]
-    allUsers = ratings[(ratings["movieId"].isin(sim_recs.index)) & (ratings["rating"] >= 4)]
+    allUsers = ratings[(ratings["movieId"].isin(sim_recs.index)) & (ratings["rating"] > 4)]
     all_recs = allUsers["movieId"].value_counts() / len(allUsers["userId"].unique())
+    rec_perc = pd.concat([sim_recs, all_recs], axis=1)
+    rec_perc.columns = ["similar", "all"]
+    rec_perc["score"] = rec_perc["similar"] / rec_perc["all"]
+    rec_perc = rec_perc.sort_values("score", ascending=False)
+    recs = rec_perc.head(10).merge(movies, left_index=True, right_on="movieId")
+    print("\nREC\n")
+    print(recs["title"])
 
 recommend("hi")
+
 search_term = input("Hello, what movie would you like to search for?\n")
 print("Here are the results:")
 print(search(search_term)["title"])
